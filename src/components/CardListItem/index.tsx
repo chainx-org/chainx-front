ff2a171c3ea521cef8a37fa9fa0ad6c413d4f7c1
xl2412 <1293358059@qq.com>
2021-11-17 11:01:09 +0800

    fix: remove polkadot/react-identicon

--- src/components/CardListItem/index.tsx
+++ src/components/CardListItem/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ItemContainer, ItemContext } from './css';
 import CopyText from '../../components/copyText';
-import Identicon from '@polkadot/react-identicon';
+// import Identicon from '@polkadot/react-identicon';
 
 interface CardListItem {
   itemTitle: string,
@@ -14,11 +14,14 @@ export default function CardListItem({itemTitle, itemIcon, itemContent, isPublic
   return (
     <ItemContainer>
       {isPublic ? <img src={itemIcon} className="imgIcon" alt=""/>
-        : <Identicon className="imgIcon"
-                     value={itemContent}
-                     size={40}
-                     theme="polkadot"
-        />}
+        :
+        // <Identicon className="imgIcon"
+        //              value={itemContent}
+        //              size={40}
+        //              theme="polkadot"
+        // />
+        ''
+      }
 
       <ItemContext>
         <span>{itemTitle}</span>
